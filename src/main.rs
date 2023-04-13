3ef1eb341d926a95c1d0c280acbe52f69280b378
XiaozheCheng <117330000+XiaozheCheng@users.noreply.github.com>
2023-04-12 23:07:51 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,28 @@
-use rand::Rng;
-use clipboard::ClipboardProvider;
-use clipboard::ClipboardContext;
+fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
+    let mut low = 0;
+    let mut high = arr.len() - 1;
+    
+    while low <= high {
+        let mid = (low + high) / 2;
+        
+        if arr[mid] == target {
+            return Some(mid);
+        } else if arr[mid] < target {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    None
+}
 
 fn main() {
-    let mut password_length = String::new();
-    let mut use_uppercase = String::new();
-    let mut use_lowercase = String::new();
-    let mut use_numbers = String::new();
-    let mut use_symbols = String::new();
-
-    println!("Enter password length:");
-    std::io::stdin().read_line(&mut password_length).unwrap();
-    let password_length: usize = password_length.trim().parse().unwrap();
-
-    println!("Include uppercase letters? (y/n)");
-    std::io::stdin().read_line(&mut use_uppercase).unwrap();
-    let use_uppercase = use_uppercase.trim().to_lowercase() == "y";
-
-    println!("Include lowercase letters? (y/n)");
-    std::io::stdin().read_line(&mut use_lowercase).unwrap();
-    let use_lowercase = use_lowercase.trim().to_lowercase() == "y";
-
-    println!("Include numbers? (y/n)");
-    std::io::stdin().read_line(&mut use_numbers).unwrap();
-    let use_numbers = use_numbers.trim().to_lowercase() == "y";
-
-    println!("Include symbols? (y/n)");
-    std::io::stdin().read_line(&mut use_symbols).unwrap();
-    let use_symbols = use_symbols.trim().to_lowercase() == "y";
-
-    let characters: Vec<char> = [
-        if use_uppercase { (65..=90) } else { (0..0) },
-        if use_lowercase { (97..=122) } else { (0..0) },
-        if use_numbers { (48..=57) } else { (0..0) },
-        if use_symbols { (33..=47).chain(58..=64).chain(91..=96).chain(123..=126) } else { (0..0) },
-    ]
-    .iter()
-    .flatten()
-    .map(|&i| i as u8 as char)
-    .collect();
-
-    let password: String = (0..password_length)
-        .map(|_| characters[rand::thread_rng().gen_range(0..characters.len())])
-        .collect();
-
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    ctx.set_contents(password).unwrap();
-
-    println!("Password generated: {}", password);
-    println!("Password copied to clipboard");
+    let arr = [1, 3, 5, 7, 9];
+    let target = 5;
+    
+    match binary_search(&arr, target) {
+        Some(index) => println!("{} found at index {}", target, index),
+        None => println!("{} not found", target),
+    }
 }
